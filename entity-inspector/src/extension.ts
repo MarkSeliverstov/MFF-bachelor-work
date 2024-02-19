88419ed61acccb57eaeac51463dcf6a553d51631
Mark Seliverstov <smdmrr00@gmail.com>
2024-02-19 23:43:38 +0100

    Use annotations reader in the extension

--- entity-inspector/src/extension.ts
+++ entity-inspector/src/extension.ts
@@ -1,12 +1,14 @@
 import * as vscode from 'vscode';
 
-import { commentsConfiguration } from './configuration';
+import { commentsConfiguration, Commands} from './configuration';
 import { initComplitions } from './complitions';
-import { getModel, saveModel } from './server';
+import { getModel, saveModel, saveAnnotations } from './server';
 import { mockInstanceModel } from './model/mocker';
-import { InstanceModel } from './model';
+import { InstanceModel, AnnotationModel } from './model';
+import { AnnotationReader } from './exporter';
 
 export let model: InstanceModel | null = null;
+let anotationModel: AnnotationModel;
 // This method is called when your extension is activated
 export async function activate(context: vscode.ExtensionContext) {
 	console.log('Yep, "entity-inspector" is now active!');
@@ -16,12 +18,35 @@ export async function activate(context: vscode.ExtensionContext) {
         commentsConfiguration.updateLanguagesDefinitions();
     }, null, context.subscriptions);
     
-    vscode.workspace.onDidSaveTextDocument((doc: vscode.TextDocument) => {
+    vscode.workspace.onDidSaveTextDocument(() => {
         // Change model
         saveModel(mockInstanceModel());
     });
 
 	initComplitions(context);
+    registerCommands(context, new AnnotationReader());
+}
+
+
+function registerCommands(
+	context: vscode.ExtensionContext, 
+	reader: AnnotationReader
+): void {
+	const runParserHandler = async () => {
+		vscode.window.withProgress({
+			location: vscode.ProgressLocation.Notification,
+			cancellable: true,
+			title: 'Parsing'
+		}, async (progress, token) => {
+			token.onCancellationRequested(() => {
+                console.log("User canceled the parsing");
+            });
+			anotationModel = await reader.parseWorkspace(progress, token);
+			progress.report({message: "exorting model to the server"});
+            saveAnnotations(anotationModel);
+		});
+	};
+	context.subscriptions.push(vscode.commands.registerCommand(Commands.runParserCmd(), runParserHandler));
 }
 
 // This method is called when your extension is deactivated
