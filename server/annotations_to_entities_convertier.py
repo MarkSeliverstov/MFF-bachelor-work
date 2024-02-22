30df1096f239ec50b1f0362b454bebdc1e58c352
Mark Seliverstov <smdmrr00@gmail.com>
2024-02-23 00:11:09 +0100

    Fix converter - use deep copy

--- server/annotations_to_entities_convertier.py
+++ server/annotations_to_entities_convertier.py
@@ -1,4 +1,5 @@
 from typing import List, Optional
+from copy import deepcopy
 
 class Property:
     def __init__(self, name: Optional[str] = None, description: Optional[str] = None):
@@ -6,16 +7,16 @@ class Property:
         self.description = description
 
 class EntityInstance:
-    def __init__(self, from_file: Optional[str] = None, identifier: Optional[str] = None, description: Optional[str] = None):
+    def __init__(self, from_file: Optional[str] = None, identifier: Optional[str] = None, description: Optional[str] = None, properties: Optional[List[Property]] = None):
         self.from_file = from_file
         self.identifier = identifier
         self.description = description
-        self.properties: List[Property] = []
+        self.properties: List[Property] = deepcopy(properties) if properties else []
 
 class Entity:
     def __init__(self, name: Optional[str] = None, instances: Optional[List[EntityInstance]] = None):
         self.name = name
-        self.instances: List[EntityInstance] = instances if instances else []
+        self.instances: List[EntityInstance] = deepcopy(instances) if instances else []
 
 class AnnotationsToEntitiesConverter:
     def __init__(self):
@@ -23,11 +24,10 @@ class AnnotationsToEntitiesConverter:
 
     def _add_instance(self, name: Optional[str], instance: EntityInstance):
         entity = next((entity for entity in self.entities if entity.name == name), None)
-        new_instance = EntityInstance(instance.from_file, instance.identifier, instance.description)
         if entity:
-            entity.instances.append(new_instance)
+            entity.instances.append(instance)
         else:
-            self.entities.append(Entity(name=name, instances=[new_instance]))
+            self.entities.append(Entity(name=name, instances=[instance]))
 
     def _convert_annotations_to_entities(self, annotations: dict) -> None:
         current_instance: Optional[EntityInstance] = None
