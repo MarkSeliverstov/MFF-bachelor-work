a6491472e17a4907c9ddc46f3e620f8a948ceafd
Mark Seliverstov <smdmrr00@gmail.com>
2024-03-30 11:25:35 +0100

    Using one config file

--- backend/app.py
+++ backend/app.py
@@ -26,6 +26,7 @@ def _convert_command(annotations_file: str, output: str) -> None:
         json.dump(entities, f, indent=4)
 
 def run():
+    config = Config.from_file("ei-config.json")
     # Commands for parse annotations, convert annotations to entities and save entities as enum
     parser = argparse.ArgumentParser(description="Parse annotations from source code and convert them to entities")
     subparsers = parser.add_subparsers(dest="command", help="commands")
@@ -34,12 +35,12 @@ def run():
 
     # Arguments for parse command
     parse_parser.add_argument("path", help="Path to file or directory to parse", type=str, default=".")
-    parse_parser.add_argument("-p", "--annotation-prefix", help="Annotation prefix", type=str, default=ANNOTATION_PREFIX)
-    parse_parser.add_argument("-o", "--output", help="Output file name", type=str, default=OUTPUT_ANNOTATIONS)
+    parse_parser.add_argument("-p", "--annotation-prefix", help="Annotation prefix", type=str, default=config.ANNOTATION_PREFIX)
+    parse_parser.add_argument("-o", "--output", help="Output file name", type=str, default=config.OUTPUT_ANNOTATIONS)
 
     # Arguments for convert command
-    convert_parser.add_argument("-a", "--annotations", type=str, help="Path to the annotations file", default=OUTPUT_ANNOTATIONS)
-    convert_parser.add_argument("-o", "--output", type=str, help="Path to the output file", default=OUTPUT_ENTITIES)
+    convert_parser.add_argument("-a", "--annotations", type=str, help="Path to the annotations file", default=config.OUTPUT_ANNOTATIONS)
+    convert_parser.add_argument("-o", "--output", type=str, help="Path to the output file", default=config.OUTPUT_ENTITIES)
 
     args = parser.parse_args()
     if args.command == "parse":
@@ -47,7 +48,7 @@ def run():
     elif args.command == "convert":
         _convert_command(args.annotations, args.output)
     else:
-        _parse_command(".", ANNOTATION_PREFIX, OUTPUT_ANNOTATIONS)
+        _parse_command(".", config.ANNOTATION_PREFIX, config.OUTPUT_ANNOTATIONS)
 
 
 if __name__ == "__main__":
--- backend/config.py
+++ backend/config.py
@@ -1,15 +1,37 @@
-ANNOTATION_PREFIX = "@lc-"
-EXTENSIONS_MAP = {
-    "ts": "application/javascript",
-    "js": "application/javascript",
-    "py": "text/x-python",
-    "html": "text/html",
-    "java": "text/x-java-source",
-    "c": "text/x-c",
-    "h": "text/x-c",
-    "cpp": "text/x-c++",
-    "hpp": "text/x-c++",
-    "xml": "text/xml",
-}
-OUTPUT_ANNOTATIONS = "annotations.json"
-OUTPUT_ENTITIES = "entities.json"
+import json
+import os
+
+class Config:
+    """
+    Config class to store the configuration of the tool
+    """
+    ANNOTATION_PREFIX = "@lc-"
+    EXTENSIONS_MAP = {
+        "ts": "application/javascript",
+        "js": "application/javascript",
+        "py": "text/x-python",
+        "html": "text/html",
+        "java": "text/x-java-source",
+        "c": "text/x-c",
+        "h": "text/x-c",
+        "cpp": "text/x-c++",
+        "hpp": "text/x-c++",
+        "xml": "text/xml",
+    }
+    OUTPUT_ANNOTATIONS = "annotations.json"
+    OUTPUT_ENTITIES = "entities.json"
+    
+    @staticmethod
+    def from_file(config_file) -> "Config":
+        """
+        Load the configuration from a file
+        """
+        if not os.path.exists(config_file): 
+            return Config()
+        with open(config_file) as f:
+            config_f = json.load(f)
+            config = Config()
+            config.ANNOTATION_PREFIX = config_f.get("prefixName", config.ANNOTATION_PREFIX)
+            config.OUTPUT_ANNOTATIONS = config_f.get("annotationsModel", config.OUTPUT_ANNOTATIONS)
+            config.OUTPUT_ENTITIES = config_f.get("entitiesModel", config.OUTPUT_ENTITIES)
+            return config
