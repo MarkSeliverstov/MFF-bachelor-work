f5bd3b16321ada68af6b67ae712f3d2d513efc2f
smdmrr <96746739+MarkSeliverstov@users.noreply.github.com>
2024-07-06 12:35:25 +0200

    fix: grammar correction (#20)

--- webapp/src/components/TheAbout.vue
+++ webapp/src/components/TheAbout.vue
@@ -2,11 +2,11 @@
     <div class="q-pa-xl" >
         <h1>About</h1>
         <p class="text-body1 text-center q-pa-xl">
-            This is website was created for visualizing annotations and entities 
-            from Entity Inspector extension server. It is built with Vue 3 and 
-            Vite. It fetches data from a REST API and displays it in a 
-            user-friendly way. So, you can easily see the structure of the 
-            entities and their annotations from your project.
+          This website was created to visualize annotations and entities from the Entity
+          Inspector extension server. It is built with Vue 3 and Vite, fetching data from
+          a REST API and displaying it in a user-friendly manner. This allows you to
+          easily view the structure of the entities and their annotations from your
+          project.
         </p>
     </div>
 </template>
